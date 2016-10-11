537c69e7481f3d649986c198dd60063a999f8e06
hoboaki <hoboaki@10106.net>
2016-10-12 00:18:15 +0900

    gleeからglewへ乗り換え。

--- Library/include/XG3D/SdkHeader.hpp
+++ Library/include/XG3D/SdkHeader.hpp
@@ -9,7 +9,8 @@
 
 #if defined(XG3D_ENGINE_GLSERIES)
     #if defined(XBASE_OS_WINDOWS)
-        #include <XG3D/GLee.h>
+        #include <XG3D/glew.h>
+        #include <XG3D/wglew.h>
         #include <gl/glu.h>
         #pragma comment(lib,"kernel32.lib")
         #pragma comment(lib,"user32.lib")
